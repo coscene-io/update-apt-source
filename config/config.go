d5bdb3c5f5c946388666a1ca53879b38a893ef88
fei.gao <104708586+YunxianHua@users.noreply.github.com>
2025-06-04 18:18:53 +0800

    feat: config check (#17)

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "slices"
+import (
+	"fmt"
+	"slices"
+)
 
 var validUbuntuDistro = []string{
 	"all",
@@ -31,19 +34,44 @@ type Config struct {
 	GpgPrivateKey   []byte
 }
 
-func (c *Config) IsValid() bool {
-	return slices.Contains(validUbuntuDistro, c.UbuntuDistro) &&
-		c.DebPaths != nil &&
-		len(c.DebPaths) > 0 &&
-		c.Architectures != nil &&
-		len(c.Architectures) > 0 &&
-		slices.Contains(validStorageTypes, c.StorageType) &&
-		c.Endpoint != "" &&
-		c.BucketName != "" &&
-		c.AccessKeyId != "" &&
-		c.AccessKeySecret != "" &&
-		c.GpgPrivateKey != nil &&
-		len(c.GpgPrivateKey) > 0
+func (c *Config) IsValid() error {
+	if !slices.Contains(validUbuntuDistro, c.UbuntuDistro) {
+		return fmt.Errorf("ubuntu distribution is not valid: %s", c.UbuntuDistro)
+	}
+	if c.DebPaths == nil {
+		return fmt.Errorf("deb paths is required: %s", c.DebPaths)
+	}
+	if len(c.DebPaths) <= 0 {
+		return fmt.Errorf("deb paths is required: %s", c.DebPaths)
+	}
+	if c.Architectures == nil {
+		return fmt.Errorf("architectures is required: %s", c.Architectures)
+	}
+	if len(c.Architectures) <= 0 {
+		return fmt.Errorf("architectures is required: %s", c.Architectures)
+	}
+	if !slices.Contains(validStorageTypes, c.StorageType) {
+		return fmt.Errorf("storage type is not valid: %s", c.StorageType)
+	}
+	if c.Endpoint == "" {
+		return fmt.Errorf("endpoint is required: %s", c.Endpoint)
+	}
+	if c.BucketName == "" {
+		return fmt.Errorf("bucket name is required: %s", c.BucketName)
+	}
+	if c.AccessKeyId == "" {
+		return fmt.Errorf("access key id is required: %s", c.AccessKeyId)
+	}
+	if c.AccessKeySecret == "" {
+		return fmt.Errorf("access key secret is required: %s", c.AccessKeySecret)
+	}
+	if c.GpgPrivateKey == nil {
+		return fmt.Errorf("gpg private key is required: %s", c.GpgPrivateKey)
+	}
+	if len(c.GpgPrivateKey) == 0 {
+		return fmt.Errorf("gpg private key is required: %s", c.GpgPrivateKey)
+	}
+	return nil
 }
 
 type SingleConfig struct {
